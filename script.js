976e01bb30d39212eca7af2502542888abb2b314
Thorbileh <preciousthobile99@gmail.com>
2024-10-08 16:06:55 +0200

    added input under the html file and code that access the number that is entered in js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+let incomeAmount =0
+
+function displayMe(){
+    const income = document.getElementById("incomeAmount").value;
+    alert("Your income " + income);
+}
+
+function displayExpens(){
+    const exp = document.getElementById("expenceAmount").value;
+    alert("Your expenses "+ exp);
+}
 
 
-function paragra(){
-    const para = document.getElementById("demo");
-    para.innerHTML="Hello world";
+function buttonDemo(){
+    const demo = document.getElementById("demo");
+    alert
+    demo.innerHTML="Hello world";
 }
