6b2133569c74e49b8e3568f6b4983250ec78418f
Thorbileh <preciousthobile99@gmail.com>
2024-10-09 08:38:07 +0200

    added the function that calculate the balance after recieving the expense and income

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-let incomeAmount =0
 
-function displayMe(){
-    const income = document.getElementById("incomeAmount").value;
-    alert("Your income " + income);
+
+
+function displayIOB(){
+   
+    
+    const income = document.getElementById("incomeAmount").value; //get the value of income that was entered by the user
+    const exp = document.getElementById("expenceAmount").value; //get the value of expense that was entered by the user
+
+    //get the the display box by id
+    let incomeDisplay =document.getElementById("incomeDisplay");
+    let expenseDisplay =document.getElementById("expenseDisplay");
+    let balanceDisplay =document.getElementById("balanceDisplay");
+
+    //calculating Balance display with income entered and expense entered
+    let balanDisplay=income-exp;
+   
+    //Displaying everything into the displayBox
+    balanceDisplay.innerHTML=balanDisplay;
+    incomeDisplay.innerHTML=income;
+    expenseDisplay.innerHTML=exp;
+
+
 }
 
-function displayExpens(){
-    const exp = document.getElementById("expenceAmount").value;
-    alert("Your expenses "+ exp);
-}
+   
 
 
-function buttonDemo(){
-    const demo = document.getElementById("demo");
-    alert
-    demo.innerHTML="Hello world";
-}
+
